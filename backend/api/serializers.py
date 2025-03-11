b1353b2f5d5ce4d58d57946f69a3a78d0672bfee
Samuel Alarcón <samuelito520@outlook.com>
2025-03-11 18:28:30 -0500

    Commit 2.0

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -8,3 +8,14 @@ class ToDoSerializer(serializers.ModelSerializer):
     class Meta:
         model = ToDo
         fields = ['id', 'title', 'memo', 'created', 'completed']
+
+class ToDoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ToDo
+        fields = ['title', 'memo', 'created', 'completed']
+
+class TodoToggleCompleteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ToDo
+        fields = []
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,12 @@
-from rest_framework import generics
-from .serializers import ToDoSerializer
+from rest_framework import generics, permissions
+from .serializers import ToDoSerializer, TodoToggleCompleteSerializer
 from todo.models import ToDo
 
 class ToDoListCreate(generics.ListCreateAPIView):
     # ListAPIView requires two mandatory attributes, serializer_class and queryset.
     # We specify TodoSerializer which we have earlier implemented
     serializer_class = ToDoSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
@@ -14,3 +15,24 @@ class ToDoListCreate(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         #serializer holds a django model
         serializer.save(user=self.request.user)
+
+class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ToDoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        # User can only update or delete their own posts
+        return ToDo.objects.filter(user=user)
+
+class TodoToggleComplete(generics.UpdateAPIView):
+    serializer_class = TodoToggleCompleteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return ToDo.objects.filter(user=user)
+
+    def perform_update(self, serializer):
+        serializer.instance.completed = not serializer.instance.completed
+        serializer.save()
